1e516b801bc13e539d33eb0bbd34509b29e4c85d
Anay Sharma <anaysharma8791@gmail.com>
2023-04-07 12:45:54 +0530

    fix separator color

--- src/components/SectionSaperator.tsx
+++ src/components/SectionSaperator.tsx
@@ -1,3 +1,7 @@
 export default function Seperator() {
-	return <div className="mx-auto my-12 h-px w-5/6 bg-slate-500"> </div>;
+	return (
+		<div className="mx-auto my-12 h-px w-5/6 bg-gray-200 dark:bg-slate-700">
+			{' '}
+		</div>
+	);
 }
