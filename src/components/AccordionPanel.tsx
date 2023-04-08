1df5f7c1b1a480efc483ec0d58ab571a142f4fd5
Anay Sharma <anaysharma8791@gmail.com>
2023-04-08 12:55:55 +0530

    change accordion panel layout

--- src/components/AccordionPanel.tsx
+++ src/components/AccordionPanel.tsx
@@ -9,12 +9,10 @@ function AccordionPanel(props: {
 	clickHandler: React.Dispatch<React.SetStateAction<number>>;
 }) {
 	return (
-		<div className="accordion-panel overflow-hidden rounded border shadow dark:border-slate-700">
+		<div className="accordion-panel overflow-hidden rounded border bg-gray-50 shadow dark:border-slate-700 dark:bg-slate-800">
 			<h2 id={`panel${props.id}-heading`}>
 				<button
-					className={`accordion-trigger flex w-full flex-row-reverse items-center justify-end gap-2 bg-gray-50 p-2 dark:bg-slate-800 ${
-						props.ariaExpanded ? 'border-b dark:border-b-slate-700' : ''
-					}`}
+					className={`accordion-trigger relative flex h-14 w-full flex-row-reverse items-center justify-end gap-2 p-2`}
 					aria-controls={`panel${props.id}-content`}
 					aria-expanded={props.ariaExpanded}
 					onClick={() => {
@@ -22,15 +20,31 @@ function AccordionPanel(props: {
 					}}
 				>
 					<span
-						className="flex w-full items-center justify-between text-left text-lg"
+						className={`flex w-full items-center justify-between text-left text-lg transition-all duration-200 ${
+							props.ariaExpanded ? 'pl-28' : 'pl-12'
+						}`}
 						id={`panel${props.id}-title`}
 					>
 						<div>
-							<h3 className="text-xl font-medium leading-6">{props.title}</h3>
-							<span className="pr-2 text-xs font-medium opacity-70">
+							<h3
+								className={`overflow-hidden text-xl font-medium transition-all duration-200 ${
+									props.ariaExpanded ? 'h-0' : 'h-6'
+								}`}
+							>
+								{props.title}
+							</h3>
+							<span
+								className={`pr-2 font-medium transition-all duration-200 ${
+									props.ariaExpanded ? 'text-lg' : 'text-xs opacity-70'
+								}`}
+							>
 								{props.name}
 							</span>
-							<span className="hidden border-l pl-2 text-xs leading-3 opacity-50 dark:border-l-slate-600 md:inline">
+							<span
+								className={`hidden text-xs leading-3 opacity-50 transition-all duration-200 dark:border-l-slate-600 ${
+									props.ariaExpanded ? 'md:block' : 'md:inline'
+								}`}
+							>
 								{props.email}
 							</span>
 						</div>
@@ -41,20 +55,22 @@ function AccordionPanel(props: {
 					</span>
 					<img
 						aria-hidden="true"
-						className="accordion-icon aspect-square h-14 rounded-full border dark:border-slate-600"
+						className={`accordion-icon absolute top-2 aspect-square rounded-full border transition-all duration-200 dark:border-slate-600 ${
+							props.ariaExpanded ? 'h-24' : ' h-10'
+						}`}
 						src={props.url}
 					/>
 				</button>
 			</h2>
 			<div
 				className={`accordion-content overflow-y-scroll transition-all  duration-200 ${
-					props.ariaExpanded ? 'h-48 md:h-36' : 'h-0'
+					props.ariaExpanded ? 'h-48 opacity-100 md:h-36' : 'h-0 opacity-0'
 				}`}
 				id={`panel${props.id}-content`}
 				aria-labelledby={`panel${props.id}-heading`}
 				role="region"
 			>
-				<p className="py-2 px-4">{props.content}</p>
+				<p className="ml-24 py-2 px-4">{props.content}</p>
 			</div>
 		</div>
 	);
